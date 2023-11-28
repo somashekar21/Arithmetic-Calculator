75f720d48141c729094efbc6236456927ff4c88c
Somashekar G <Somashekargandikota@gmail.com>
2023-11-28 15:32:06 +0530

    Calculator

---
+++ src/calculator/CalculatorProgram.java
@@ -0,0 +1,59 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class CalculatorProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get input from the user
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Display the menu
+        System.out.println("\nSelect an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        // Get the choice from the user
+        System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+
+        // Perform the selected operation
+        double result = 0;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
